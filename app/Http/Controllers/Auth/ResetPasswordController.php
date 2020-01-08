74a40b4ddc03056f1827ec79a87a6eb473db24ee
gahamba <58950015+gahamba@users.noreply.github.com>
2020-01-08 15:42:55 +0000

    Updated error with reset password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use MongoDB\BSON\UTCDateTime;
 
 class ResetPasswordController extends Controller
 {
